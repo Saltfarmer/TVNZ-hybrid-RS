83c7fea01bbe04863df2e2e4106004d293ced035
Candra Gctk <candragctk17@gmail.com>
2022-03-31 10:08:23 +0200

    Finish plotting
    Need to fix deprecation

--- app.py
+++ app.py
@@ -2,15 +2,19 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import random
-
+import seaborn as sns
 
 st.set_page_config(layout="wide")
 
-df = pd.read_csv('TVNZ_movies_v3.csv')
+content = pd.read_csv('TVNZ_movies_v3.csv')
+user = pd.read_csv('user_data.csv')
+rating = pd.read_csv('rating_data.csv')
+
+combine = content.merge(rating.merge(user, left_on=['user_id'], right_on=['ID']), left_on=['ID'], right_on=['content_id'] ) 
 
 # Choosing random default key in state session
 if 'key' not in st.session_state:
-  st.session_state['key'] = random.randint(0, len(df))
+  st.session_state['key'] = random.randint(0, len(content))
 
 key = st.session_state['key']
 
@@ -20,26 +24,27 @@ cover, info = st.columns([3, 2])
 
 with cover:
   # display the image
-  st.image(df['image'].iloc[key])
+  st.image(content['image'].iloc[key])
 
 with info:
   # display the book information
-  st.title(df['title'].iloc[key])
-  st.header("Genre : " + df['GenreType'].iloc[key])
-  st.markdown(df['description'].iloc[key])  
-  st.markdown(df['distributor'].iloc[key])  
-  st.caption("genre : " + df['genre'].iloc[key])
+  st.title(content['title'].iloc[key])
+  st.header("Genre : " + content['GenreType'].iloc[key])
+  st.markdown(content['description'].iloc[key]) 
+  st.caption("genre : " + content['genre'].iloc[key])
 
-user_review, rating_hist = st.columns([2, 2])
+user_review, rating_hist = st.columns([2, 3])
+
+# Create a data based on specific content
+user_watch_content = combine[combine['content_id'] == content['ID'].iloc[key]]
+
+# Create a sample comment to show
+sample = user_watch_content.sample(1)
 
 with user_review:
-  st.title("User")
-  st.markdown("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
-
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
+  st.title(sample['username'].values[0])
+  st.markdown(sample['comment'].values[0])
 
 with rating_hist:
-  st.area_chart(chart_data)
-
+  sns.countplot(user_watch_content['rating'])
+  st.pyplot()
